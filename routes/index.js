40eefc44175f0500ab4a4b40a39c9aff012e17b5
bernadette privateer <lynprivateer@gmail.com>
2017-08-18 11:31:14 -0400

    final is good name just needs to be changed

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ function auth(req, res, next){
 };
 
 router.get('/', auth, function(req, res){
-  res.render("login");
+  res.render("login", {errors: req.session.errors});
 });
 
 router.get("/home", function(req, res, next) {
@@ -35,24 +35,19 @@ req.checkBody("username", "name must not be empty").notEmpty();
 req.checkBody("password", "must not be empty").notEmpty();
 req.checkBody("password", "Password must be 8 letters long").isLength({min:8});
 
+let messages = [];
+
 let errors = req.getValidationResult().then(function(error) {
   // console.log(error.array());
   error.array().forEach(function(error){
     messages.push(error.msg)
     // console.log(error.msg);
-    console.log(messages);
+    console.log('messages\n', messages);
   })
-  // console.log(error.array(param));
-});
-// let errors = req.getValidationResult();
-
-let messages = [];
-
-let erOb = {
-  error: errors,
-  errors: messages,
-  info: req.body,
-}
+  if (messages.length > 0){
+    req.session.errors = messages;
+    res.redirect('/');
+  }
 
   let user = {
 
@@ -60,15 +55,31 @@ let erOb = {
     password: req.body.password,
   };
 
-  if(user.username == dummy_user.username && user.password == dummy_user.password){
+  if (user.username === dummy_user.username && user.password === dummy_user.password){
    req.session.dummy_user = user;
    req.session.token = "token";
    console.log(user.username);
 
    res.redirect('/home');
   } else {
-    res.render("login", erOb);
+    req.session.errors = ["Incorrect Login"];
+    res.redirect("/");
   }
+  // console.log(error.array(param));
+});
+// let errors = req.getValidationResult();
+
+
+//
+// let errObj = {
+//   error: errors,
+//   errors: messages,
+//   info: req.body,
+// }
+
+// console.log('errObj: ', errObj);
+
+
 })
 
 router.get("/logout", function(req, res) {
